7c9add3178aa4ea4f2fde65d73d7ceb5c1b2637c
CohenOreilly <80442810+CohenOreilly@users.noreply.github.com>
2021-05-06 16:11:12 +1000

    submisson

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assessment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         Console.WriteLine("do u want to roll Yes/No");
+
+             while (true)
+             {
+             string ans = Console.ReadLine();
+              if (ans != null && ans == "y")
+                 {
+                 Random rnd = new Random();
+                 int dice   = rnd.Next(1, 7);       
+                 Console.WriteLine(dice);
+                 }
+              else if ( ans != null && ans == "n")
+                 {
+
+                 }
+              else
+                {
+                  Console.Write("Only y or n Allowed");
+
+                }
+             }
+
+        }  
+    }
+}
